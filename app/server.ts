fbccf959893cb02754c3c7c6725f2d7e530dcb1b
shermondevelopment <victorshermon@yahoo.com>
2022-12-09 19:58:46 -0300

    feat: parsing the request body in json

--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { json } from 'express'
 
 /** server */
 const app = express()
+app.use(json())
 
 app.listen(3000, () => console.log('app running in port 3000'))
