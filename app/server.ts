87628608152b5d0d58932e8023a18f19f313415b
shermondevelopment <victorshermon@yahoo.com>
2022-12-09 20:00:57 -0300

    chore: including library of cors in project

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,12 @@
+/** express */
 import express, { json } from 'express'
+
+/** cors */
+import cors from 'cors'
 
 /** server */
 const app = express()
 app.use(json())
+app.use(cors())
 
 app.listen(3000, () => console.log('app running in port 3000'))
