ee3829d0faaf33e800860e00f7133f371f4eedc9
shermondevelopment <victorshermon@yahoo.com>
2022-12-10 08:42:50 -0300

    feat: implementing validator of list in router /new/list

--- app/routers/listRouter.ts
+++ app/routers/listRouter.ts
@@ -3,9 +3,15 @@ import { Router } from 'express'
 /** controllers */
 import { newListController } from '../controllers/listController'
 
+/** middleware */
+import bodyValidatory from '../middlewares/bodyValidatory'
+
+/** validation scheme for list */
+import { listValidator } from '../validators/listValidator'
+
 /** router of list */
 const listRouter = Router()
 
-listRouter.post('/new/list', newListController)
+listRouter.post('/new/list', bodyValidatory(listValidator), newListController)
 
 export default listRouter
