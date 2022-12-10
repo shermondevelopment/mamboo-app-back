7ef0436e11bd4efc3d79be37f619854e722fa929
shermondevelopment <victorshermon@yahoo.com>
2022-12-10 08:04:22 -0300

    feat: configuring connection with mongodb

--- app/server.ts
+++ app/server.ts
@@ -12,6 +12,13 @@ const app = express()
 app.use(json())
 app.use(cors())
 
-app.listen(process.env.PORT_APP_MAMBOO || 3001, () =>
-  console.log(`app running in port ${process.env.PORT_APP_MAMBOO} 🚀🚀🚀`)
-)
+/** connection with db */
+import connectWithMongoDb from './db/connect'
+
+connectWithMongoDb
+  .then(() => {
+    app.listen(process.env.PORT_APP_MAMBOO || 3001, () =>
+      console.log(`app running in port ${process.env.PORT_APP_MAMBOO} 🚀🚀🚀`)
+    )
+  })
+  .catch(() => console.log('connection failed ❌❌❌'))
